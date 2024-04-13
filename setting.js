e2d224599efe4895adb6739624d068c028090d60
tder6 <1218546874@qq.com>
2024-04-13 20:27:15 +0800

    v1.4.1.1 upd style

--- setting.js
+++ setting.js
@@ -6,7 +6,7 @@ changeSize = function() {
     else h2 = "0px";
     var n1 = Number(h1.substr(0, h1.length - 2));
     var n2 = Number(h2.substr(0, h2.length - 2));
-    var height = Math.max(n1 + n2 + 15, document.getElementsByClassName("whole")[0].scrollHeight - 170);
+    var height = Math.max(n1 + n2 + 22.5, document.getElementsByClassName("whole")[0].scrollHeight - 170);
     document.getElementsByClassName("article")[0].style.height = height.toString() + "px"; 
     document.getElementsByClassName("menu")[0].style.height = height.toString() + "px"; 
 }
