95ab79ae8ff23257757654efbc9a95b4f82a44b2
tder <149858418+tder6@users.noreply.github.com>
2024-04-13 15:24:44 +0800

    v1.3.2 upd script
    - Upd script.
    - Fix Issue #4.

---
+++ setting.js
@@ -0,0 +1,20 @@
+changeSize = function() {
+    document.body.style.backgroundSize = Math.max(document.body.scrollWidth, 1500).toString() + "px"; 
+    var h1 = window.getComputedStyle(document.getElementsByClassName("text")[1]).height;
+    var h2;
+    if(document.getElementById("markdown") != undefined) h2 = window.getComputedStyle(document.getElementById("markdown")).height;
+    else h2 = "0px";
+    // console.log(h1, h2);
+    var n1 = Number(h1.substr(0, h1.length - 2));
+    var n2 = Number(h2.substr(0, h2.length - 2));
+    // console.log(n1, n2);
+    var height = Math.max(n1 + n2 + 15, document.getElementsByClassName("whole")[0].scrollHeight - 170);
+    document.getElementsByClassName("article")[0].style.height = height.toString() + "px"; 
+    document.getElementsByClassName("menu")[0].style.height = height.toString() + "px"; 
+}
+window.onload = function() {
+    changeSize();
+}
+window.onresize = function() {
+    changeSize();
+}
