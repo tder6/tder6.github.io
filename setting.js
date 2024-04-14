6d2ff12c2c5b2a29464520ca4ce46e1c9a113ddd
tder6 <1218546874@qq.com>
2024-04-14 13:56:04 +0800

    v1.5.0.3 test git
    - Test git.

--- setting.js
+++ setting.js
@@ -1,11 +1,9 @@
 changeSize = function() {
     document.body.style.backgroundSize = Math.max(document.body.scrollWidth, 1500).toString() + "px"; 
-    var titleHeight = window.getComputedStyle(document.getElementsByClassName("text")[1]).height;
-    var textHeight;
+    var titleHeight = window.getComputedStyle(document.getElementsByClassName("text")[1]).height, textHeight;
     if(document.getElementById("markdown") != undefined) textHeight = window.getComputedStyle(document.getElementById("markdown")).height;
     else textHeight = "0px";
-    var titleHeightNumber = Number(titleHeight.substr(0, titleHeight.length - 2));
-    var textHeightNumber = Number(textHeight.substr(0, textHeight.length - 2));
+    var titleHeightNumber = Number(titleHeight.substr(0, titleHeight.length - 2)), textHeightNumber = Number(textHeight.substr(0, textHeight.length - 2));
     var totalHeight = Math.max(titleHeightNumber + textHeightNumber + 22.5, document.getElementsByClassName("whole")[0].scrollHeight - 170);
     document.getElementsByClassName("article")[0].style.height = totalHeight.toString() + "px"; 
     document.getElementsByClassName("menu")[0].style.height = totalHeight.toString() + "px"; 
