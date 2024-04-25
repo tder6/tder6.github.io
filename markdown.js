b195f101ad71cd75062010bf43ebae4ec84a0ec1
tder6 <1218546874@qq.com>
2024-04-25 11:25:14 +0800

    v1.5.9 upd script
    - Upd script.

--- markdown.js
+++ markdown.js
@@ -2,7 +2,20 @@ var markdownFile = new XMLHttpRequest();
 markdownFile.open("GET", "/articles/" + title + ".md", true);
 markdownFile.onreadystatechange = function() {
     if(markdownFile.readyState === 4 && markdownFile.status === 200) {
-        var markdownText = marked.parse(markdownFile.responseText), resultText = "";
+		var readText = markdownFile.responseText, resultText = "";
+		for(var i = 0, b1 = 1, b2 = 1; i < readText.length; i++) {
+            if(readText[i] === '\\' && (!b1 || !b2)) resultText += "\\\\";
+			else if(readText[i] === '~' && (!b1 || !b2)) resultText += "\\~";
+			else resultText += readText[i];
+			if(readText[i] !== '$') continue;
+            else if(i < readText.length - 1 && readText[i + 1] !== '$') b1 = !b1;
+            else if(i < readText.length - 1 && readText[i + 1] === '$') {
+                b2 = !b2;
+                i++;
+				resultText += readText[i];
+            }
+        }
+        var markdownText = marked.parse(resultText); resultText = "";
         for(var i = 0, b1 = 1, b2 = 1; i < markdownText.length; i++) {
             if(i < markdownText.length - 2 && markdownText.substr(i, 3) === "<a ") {
 				resultText += "<a class=\"link\" ";
@@ -16,7 +29,7 @@ markdownFile.onreadystatechange = function() {
                 if(b1) resultText += "\\(";
                 else resultText += "\\)";
                 b1 = !b1;
-            } else if(markdownText[i + 1] === '$') {
+            } else if(i < markdownText.length - 1 && markdownText[i + 1] === '$') {
                 if(b2) resultText += "\\[";
                 else resultText += "\\]";
                 b2 = !b2;
